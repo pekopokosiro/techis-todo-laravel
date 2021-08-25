e8210795b8835dad0d9c061f14b01445c8b5ee74
pekopokosiro <soniccat01@gmail.com>
2021-08-25 23:26:33 +0900

    t

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,9 @@ class AppServiceProvider extends ServiceProvider
     
     {
         Schema::defaultStringLength(191);
-        if (\App::environment(['production'])){
+        if (\App::environment(['production']) || \App::environment(['develop'])){
             \URL::forceScheme('https');
+
         }
     }
 
